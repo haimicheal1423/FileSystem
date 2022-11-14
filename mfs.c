fe8a1527c7497c50a18dae26b96d6ffa2e6a1360
Student <mhua1@sfsu.edu>
2022-11-14 10:22:22 -0800

    made mfs.c file

---
+++ mfs.c
@@ -0,0 +1,14 @@
+/**************************************************************
+* Class:  CSC-415-02 Fall 2022
+* Names: Sajan Gurung, Michael Hua, Deep Dhorajiya, Ron Elijah Rivera
+* Student IDs: 921149577, 918729206, 920842114, 921656746
+* GitHub Name: michealhuaa
+* Group Name: Team Buffer
+* Project: Basic File System
+*
+* File: mfs.c
+*
+* Description: Makes functions that interacts with the file system
+*
+*
+ **************************************************************/
